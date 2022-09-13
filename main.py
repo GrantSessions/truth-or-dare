b6cd2c7aa6ff00058804adc846b40f1d52787ace
GrantSessions <112982031+GrantSessions@users.noreply.github.com>
2022-09-13 12:47:45 -0600

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,20 @@
+global number
+basic.show_string("Truth Or Dare!")
+def on_button_pressed_a():
+    number = randint(0, 1)
+    if number == 0:
+        basic.show_string("Truth")
+    elif number == 1:
+        basic.show_string("Dare")
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def b():
+    number = randint(0, 3)
+    if number == 0:
+        basic.show_arrow(ArrowNames.NORTH)
+    elif number == 1:
+        basic.show_arrow(ArrowNames.SOUTH)
+    elif number == 2:
+        basic.show_arrow(ArrowNames.EAST)
+    else:
+        basic.show_arrow(ArrowNames.WEST)
---
+++ main.ts
@@ -0,0 +1,25 @@
+
+basic.showString("Truth Or Dare!")
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    let number = randint(0, 1)
+    if (number == 0) {
+        basic.showString("Truth")
+    } else if (number == 1) {
+        basic.showString("Dare")
+    }
+    
+})
+function b() {
+    let number = randint(0, 3)
+    if (number == 0) {
+        basic.showArrow(ArrowNames.North)
+    } else if (number == 1) {
+        basic.showArrow(ArrowNames.South)
+    } else if (number == 2) {
+        basic.showArrow(ArrowNames.East)
+    } else {
+        basic.showArrow(ArrowNames.West)
+    }
+    
+}
+
